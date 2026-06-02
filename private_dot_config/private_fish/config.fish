8e3279b36fd31d125094eb9c67a85a2194211947
Medi <m.m.mohammadi99@gmail.com>
2026-06-02 19:55:58 -0400

    feat: update Fish config and add Tmuxinator project files

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,25 +2,35 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# macOS-specific paths and tools
+if test (uname) = "Darwin"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
-alias "mux"="tmuxinator"
+    alias "mux"="tmuxinator"
 
-fish_add_path /opt/homebrew/opt/ruby/bin
+    fish_add_path /opt/homebrew/opt/ruby/bin
 
-set -x ANDROID_HOME $HOME/Library/Android/sdk
-fish_add_path $ANDROID_HOME/emulator
-fish_add_path $ANDROID_HOME/platform-tools
-fish_add_path $ANDROID_HOME/tools
-fish_add_path $ANDROID_HOME/tools/bin
+    set -x ANDROID_HOME $HOME/Library/Android/sdk
+    fish_add_path $ANDROID_HOME/emulator
+    fish_add_path $ANDROID_HOME/platform-tools
+    fish_add_path $ANDROID_HOME/tools
+    fish_add_path $ANDROID_HOME/tools/bin
 
-set -x JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+    set -x JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
 
-fish_add_path /opt/podman/bin
+    fish_add_path /opt/podman/bin
 
-set -x GEM_HOME (ruby -e 'puts Gem.user_dir' 2>/dev/null)
-if test -n "$GEM_HOME"
-    fish_add_path $GEM_HOME/bin
+    set -x GEM_HOME (ruby -e 'puts Gem.user_dir' 2>/dev/null)
+    if test -n "$GEM_HOME"
+        fish_add_path $GEM_HOME/bin
+    end
+
+    # tmuxinator helper: symlink repo tmuxinator configs to ~/.tmuxinator if missing
+    if not test -e $HOME/.tmuxinator
+        if test -d $HOME/Documents/Projects/dotenv/private_dot_config/tmuxinator
+            ln -s $HOME/Documents/Projects/dotenv/private_dot_config/tmuxinator $HOME/.tmuxinator
+        end
+    end
 end
 
 fzf --fish | source
---
+++ private_dot_config/tmuxinator/README.md
@@ -0,0 +1,5 @@
+Tmuxinator project files for this dotfiles repo.
+
+Place tmuxinator YAML project files here. On macOS the Fish config will symlink this directory to ~/.tmuxinator.
+
+Example: `example.yml` is included as a starter project.
